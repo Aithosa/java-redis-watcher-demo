eec758deb725be5be217a6b2e8c793242e00bc5a
Aithusa <32020141+Aithosa@users.noreply.github.com>
2024-12-09 18:13:54 +0800

    init commit

---
+++ src/main/java/org/example/rediswatcher/RedisWatcherDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.rediswatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisWatcherDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisWatcherDemoApplication.class, args);
+    }
+
+}
